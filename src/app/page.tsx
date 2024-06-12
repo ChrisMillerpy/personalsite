414846520cddc17547f0cc5d0c904e7f606a3b4c
ChrisMillerpy <christophorusmiller@gmail.com>
2024-06-12 21:12:09 +0100

    Setup basic page.tsx to and setup basic header shape

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-darkred-background">
-      <h1>Personal Website</h1>
+    <main className="min-h-screen bg-darkred-background px-36">
+      <Header />
     </main>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="absolute top-0 left-0 bg-darkred-header opacity-[0.13] w-full h-[90px] z-50">
+      Header
+    </div>
+  );
 };
 
 export default Header;
