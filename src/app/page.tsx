31cdbf45612adaae3d67051a5f1485f77165a7c6
ChrisMillerpy <christophorusmiller@gmail.com>
2024-06-12 03:09:49 +0100

    Set background red

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-myred">
       <h1>Personal Website</h1>
     </main>
   );
