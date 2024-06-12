32d766b785c02b63d6ff288bd62c83f4b9c9f148
ChrisMillerpy <christophorusmiller@gmail.com>
2024-06-12 22:10:29 +0100

    Added basic hardcoded header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Header from "@/components/Header";
 
 export default function Home() {
+  const xlPadding: string = "px-36";
   return (
-    <main className="min-h-screen bg-darkred-background px-36">
-      <Header />
+    <main className={`min-h-screen bg-darkred-background ${xlPadding}`}>
+      <Header padding={xlPadding} />
     </main>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,30 @@
-const Header = () => {
+interface HeaderProps {
+  padding?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ padding }) => {
   return (
-    <div className="absolute top-0 left-0 bg-darkred-header opacity-[0.13] w-full h-[90px] z-50">
-      Header
+    <div
+      className={`absolute flex items-center top-0 left-0 bg-darkred-header bg-opacity-[0.13] w-full h-[90px] ${padding}`}
+    >
+      <div className="w-full flex justify-between items-center">
+        <div>
+          <h2 className="uppercase">Chris Miller</h2>
+        </div>
+        <div>
+          <ul className="flex">
+            <li className="ml-20">
+              <a href="">projects</a>
+            </li>
+            <li className="ml-20">
+              <a href="">blog</a>
+            </li>
+            <li className="ml-20">
+              <a href="">contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
