68288bc159846ac43912b5b2973a652d6ecf0b36
ChrisMillerpy <christophorusmiller@gmail.com>
2024-06-23 22:08:05 +0100

    Added id to placeholder section

--- src/components/Placeholder.tsx
+++ src/components/Placeholder.tsx
@@ -1,10 +1,12 @@
+import Section from "./Section";
+
 const Placeholder = () => {
   return (
-    <>
+    <Section id="placeholder">
       <div className="my-10 rounded-xl h-40 bg-slate-300/30"></div>
       <div className="my-10 rounded-xl h-40 bg-slate-300/30"></div>
       <div className="my-10 rounded-xl h-40 bg-slate-300/30"></div>
-    </>
+    </Section>
   );
 };
 
