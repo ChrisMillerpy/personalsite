6f860147f36fb93cb7515a63abbc33137f39c84c
ChrisMillerpy <christophorusmiller@gmail.com>
2024-06-23 18:56:04 +0100

    Mocked up a quick project card component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,19 +1,31 @@
 import Section from "./Section";
 
 interface ProjectCardProps {
-  children?: any;
+  name?: string;
+  description?: string;
 }
 
-const ProjectCard: React.FC<ProjectCardProps> = ({ children }) => {
-  return <div>{children}</div>;
+const ProjectCard: React.FC<ProjectCardProps> = ({ name, description }) => {
+  return (
+    <div>
+      <div className="border-project-base border-2 rounded-t-xl py-10 px-5">
+        <h3 className="text-center">Project Preview Photo</h3>
+      </div>
+      <div className="bg-project-base rounded-b-xl pt-2 px-4 pb-10">
+        <h4 className="mb-2">{name}</h4>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
 };
 
 const Projects = () => {
   return (
     <Section className="grid grid-cols-3 gap-[15px]">
-      <div>
-        <ProjectCard></ProjectCard>
-      </div>
+      <ProjectCard name="Project 1" description="Description of project 1" />
+      <ProjectCard name="Project 2" description="Description of project 2" />
+      <ProjectCard name="Project 3" description="Description of project 3" />
+      <ProjectCard name="Project 4" description="Description of project 4" />
     </Section>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,9 @@ const config: Config = {
           2: "#510018",
           3: "#FF006B",
         },
+        project: {
+          base: "#D7224D",
+        },
       },
       fontFamily: {
         outfit: ["Outfit", "sans-serif"],
