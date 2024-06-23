4a6dfd1698ee8fedd642fd66b20d680ddde23a8d
ChrisMillerpy <christophorusmiller@gmail.com>
2024-06-23 18:54:26 +0100

    Slightly changed the classNames since text-white/85 was redundant now

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,15 +3,15 @@ import Section from "./Section";
 const Hero = () => {
   return (
     <Section className="grid grid-cols-2 gap-[15px]">
-      <div className="">
+      <div>
         <h1 className="mb-12">Explore My Projects And Blog...</h1>
-        <p className="w-5/6 mb-3 text-white/85">
+        <p className="w-5/6 mb-3">
           My name's Chris. I'm a maths student and coding enthusiast. Please
           feel free to have a poke around my projects and blog. I am also very
           sexy with a wonderful moustache, my lost sector receives 5 star
           reviews all around, inside and out.
         </p>
-        <p className="text-white/85">Enjoy and have a nice day :)</p>
+        <p className="">Enjoy and have a nice day :)</p>
       </div>
       <div className="bg-white/30 rounded-3xl flex items-center justify-center">
         <h2>Logo Here</h2>
