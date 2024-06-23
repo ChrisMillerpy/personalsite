41a44da670842dfa894889f7e681bfbbe364bafb
ChrisMillerpy <christophorusmiller@gmail.com>
2024-06-23 12:21:33 +0100

    Roughed in the hero and reformated the placeholders and navbar typesetting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header: React.FC<HeaderProps> = ({ padding }) => {
       className={`fixed flex items-center justify-between top-0 left-0 bg-darkred-header bg-opacity-35 backdrop-filter backdrop-blur-lg w-full h-[90px] ${padding} z-50`}
     >
       <div>
-        <h2 className="uppercase font-outfit font-bold text-xl">
+        <h2 className="uppercase font-bold text-[24px] tracking-normal">
           Chris Miller
         </h2>
       </div>
---
+++ src/components/Hero.tsx
@@ -0,0 +1,19 @@
+const Hero = () => {
+  return (
+    <div className="grid grid-cols-2 gap-[15px] mt-20">
+      <div className="">
+        <h1 className="mb-12">Explore My Projects And Blog...</h1>
+        <p className="w-5/6 mb-3">
+          My name's Chris. I'm a maths student and coding enthusiast. Please
+          feel free to have a poke around my projects and blog.
+        </p>
+        <p>Enjoy and have a nice day :)</p>
+      </div>
+      <div className="bg-white/30 rounded-3xl flex items-center justify-center">
+        <h2>Logo Here</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
