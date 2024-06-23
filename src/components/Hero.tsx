ebb79e5ff58b1d4de28e59b8ca4083524d004f95
ChrisMillerpy <christophorusmiller@gmail.com>
2024-06-23 22:28:09 +0100

    Escaped apostrophies on Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,12 +6,12 @@ const Hero = () => {
       <div>
         <h1 className="mb-12">Explore My Projects And Blog...</h1>
         <p className="w-5/6 mb-3">
-          My name's Chris. I'm a maths student and coding enthusiast. Please
-          feel free to have a poke around my projects and blog. I am also very
-          sexy with a wonderful moustache, my lost sector receives 5 star
+          My name&apos;s Chris. I&apos;m a maths student and coding enthusiast.
+          Please feel free to have a poke around my projects and blog. I am also
+          very sexy with a wonderful moustache, my lost sector receives 5 star
           reviews all around, inside and out.
         </p>
-        <p className="">Enjoy and have a nice day :)</p>
+        <p>Enjoy and have a nice day :)</p>
       </div>
       <div className="bg-white/30 rounded-3xl flex items-center justify-center">
         <h2>Logo Here</h2>
