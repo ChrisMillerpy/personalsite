9ce74e7a61abae086f1546ac28f9990db019d27c
ChrisMillerpy <christophorusmiller@gmail.com>
2024-06-25 17:49:19 +0100

    Made contact section linear gradient to bg-darkred-background and formatted spacing of text

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,10 @@ import Section from "./Section";
 
 const Contact = () => {
   return (
-    <div id="contact" className="bg-black relative bottom-0 left-0">
+    <div
+      id="contact"
+      className="bg-black bg-gradient-to-t from-black to-darkred-background pt-[180px] pb-[90px] flex flex-col justify-center items-center gap-5"
+    >
       <h1>Drop me a message!</h1>
       <p>chris@pacemaker.blog</p>
     </div>
