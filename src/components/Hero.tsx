881a7f92a432003b7301934a19f024c5c9584611
ChrisMillerpy <christophorusmiller@gmail.com>
2024-06-29 19:56:32 +0100

    Changed a couple things with how Hero looks on smaller screens

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,9 @@ const Hero = () => {
   return (
     <Section id="hero" className="md:grid md:grid-cols-2 md:gap-[15px]">
       <div>
-        <h1 className="mb-12">Explore My Projects And Blog...</h1>
+        <h1 className="mb-12 text-center md:text-left">
+          Explore My Projects And Blog...
+        </h1>
         <p className="md:w-5/6 mb-3">
           My name&apos;s Chris. I&apos;m a maths student and coding enthusiast.
           Please feel free to have a poke around my projects and blog. I am also
