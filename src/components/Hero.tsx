3244e443d5883428dca352c6c69e1a6d7b931dcd
ChrisMillerpy <christophorusmiller@gmail.com>
2024-06-29 19:53:18 +0100

    Made Hero.tsx responsive to all screen sizes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,10 +2,10 @@ import Section from "./Section";
 
 const Hero = () => {
   return (
-    <Section id="hero" className="grid grid-cols-2 gap-[15px]">
+    <Section id="hero" className="md:grid md:grid-cols-2 md:gap-[15px]">
       <div>
         <h1 className="mb-12">Explore My Projects And Blog...</h1>
-        <p className="w-5/6 mb-3">
+        <p className="md:w-5/6 mb-3">
           My name&apos;s Chris. I&apos;m a maths student and coding enthusiast.
           Please feel free to have a poke around my projects and blog. I am also
           very sexy with a wonderful moustache, my lost sector receives 5 star
@@ -13,7 +13,7 @@ const Hero = () => {
         </p>
         <p>Enjoy and have a nice day :)</p>
       </div>
-      <div className="bg-white/30 rounded-3xl flex items-center justify-center">
+      <div className="hidden md:flex bg-white/30 rounded-3xl items-center justify-center">
         <h2>Logo Here</h2>
       </div>
     </Section>
