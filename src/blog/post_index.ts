ec85e175dd50f6079557cd328ac2b5d637462b9c
ChrisMillerpy <christophorusmiller@gmail.com>
2024-07-25 15:11:41 +0100

    Created blog file storage

---
+++ src/blog/post_index.ts
@@ -0,0 +1,12 @@
+export const posts = [
+  {
+    id: 0,
+    title: "Test Post",
+    filename: "0_test_post.mdx",
+  },
+  {
+    id: 1,
+    title: "First Post",
+    filename: "1_first_post.mdx",
+  },
+];
