17f3faa5552c456c021f46730bac99b92b73deec
ChrisMillerpy <christophorusmiller@gmail.com>
2024-07-25 15:10:32 +0100

    Imported packages to process the mdx files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mdx$/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: "raw-loader",
+          // options: pluginOptions.options,
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
