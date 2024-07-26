cdc908fe3d89f8667c3081d4a858e503a38ac532
ChrisMillerpy <christophorusmiller@gmail.com>
2024-07-26 15:36:59 +0100

    Created stand alone PostViewer.tsx component

---
+++ src/components/PostViewer.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import Markdown from "react-markdown";
+
+interface PostViewerProps {
+  slug?: string;
+}
+
+const PostViewer: React.FC<PostViewerProps> = ({ slug }) => {
+  const [content, setContent] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    import(`../../public/blog_posts/blog_files/${slug}.mdx`)
+      .then((res) => setContent(res.default))
+      .catch((err) => console.log(err));
+  });
+
+  return <Markdown children={content} />;
+};
+
+export default PostViewer;
