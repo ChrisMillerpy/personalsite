b9bba5abd701be45675e8a041af68ba712fa6f9f
ChrisMillerpy <christophorusmiller@gmail.com>
2024-07-26 15:41:07 +0100

    Altered blog section of main site to link to the appropriate page

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,20 +1,26 @@
-import { ReactNode } from "react";
+"use client";
+import { ReactNode, useEffect, useState } from "react";
 import Section from "./Section";
+import { preferences, posts } from "../../public/blog_posts/post_index";
+import PostViewer from "./PostViewer";
 
 interface BentoCardProps {
   className?: string;
   children?: ReactNode;
+  slug?: string;
 }
 
-const BentoCard: React.FC<BentoCardProps> = ({ className, children }) => {
+const BentoCard: React.FC<BentoCardProps> = ({ className, children, slug }) => {
   return (
-    <div
+    <a
       className={`bg-white/20 rounded-lg ${
         className || ""
       } p-4 hover:bg-white/30 transition-colors`}
+      href={`/blog/${slug}`}
     >
-      {children}
-    </div>
+      {/* {children} */}
+      <PostViewer slug={slug} />
+    </a>
   );
 };
 
@@ -31,22 +37,39 @@ const MoreBlogPostsButton = () => {
 };
 
 const Blog = () => {
+  const firstFeaturedSlug = posts[preferences.firstFeaturedId].fileName;
+  const secondFeaturedSlug = posts[preferences.secondFeaturedId].fileName;
+  const mostRecentSlug = posts[posts.length - 1].fileName;
+  const secondMostRecentSlug = posts[posts.length - 2].fileName;
+
   return (
     <Section id="blog">
       <div className="md:grid md:grid-cols-3 md:grid-rows-12 flex flex-col gap-[15px]">
-        <BentoCard className="md:col-start-1 md:col-end-3 md:row-start-1 md:row-end-9 h-[32rem] md:h-96">
-          <h3 className="mb-3">Most Recent Title</h3>
-          <p>this is the text for the most recent blog post</p>
+        <BentoCard
+          className="md:col-start-1 md:col-end-3 md:row-start-1 md:row-end-9 h-[32rem] md:h-96"
+          slug={mostRecentSlug}
+        >
+          {/* <h3 className="mb-3">Most Recent Title</h3>
+          <p>this is the text for the most recent blog post</p> */}
         </BentoCard>
-        <BentoCard className="md:col-start-1 md:col-end-3 md:row-start-9 md:row-end-13 h-[32rem] md:h-auto">
-          <h4>Second Most Recent Title</h4>
+        <BentoCard
+          className="md:col-start-1 md:col-end-3 md:row-start-9 md:row-end-13 h-[32rem] md:h-auto"
+          slug={secondMostRecentSlug}
+        >
+          {/* <h4>Second Most Recent Title</h4> */}
         </BentoCard>
 
-        <BentoCard className="md:col-start-3 md:col-end-4 md:row-start-1 md:row-end-7 h-[32rem] md:h-auto">
-          <h4>Featured 1 Title</h4>
+        <BentoCard
+          className="md:col-start-3 md:col-end-4 md:row-start-1 md:row-end-7 h-[32rem] md:h-auto"
+          slug={firstFeaturedSlug}
+        >
+          {/* <h4>Featured 1 Title</h4> */}
         </BentoCard>
-        <BentoCard className="md:col-start-3 md:col-end-4 md:row-start-7 md:row-end-13 h-[32rem] md:h-auto">
-          <h4>Featured 2 Title</h4>
+        <BentoCard
+          className="md:col-start-3 md:col-end-4 md:row-start-7 md:row-end-13 h-[32rem] md:h-auto"
+          slug={secondFeaturedSlug}
+        >
+          {/* <h4>Featured 2 Title</h4> */}
         </BentoCard>
       </div>
 
