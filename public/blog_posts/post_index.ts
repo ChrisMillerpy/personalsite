2e15cfb0abba1f2f0403e5a972c9b5aa0d991059
ChrisMillerpy <christophorusmiller@gmail.com>
2024-07-26 15:35:59 +0100

    Moved the files for blog posts from src to public

---
+++ public/blog_posts/post_index.ts
@@ -0,0 +1,35 @@
+export const preferences = {
+  firstFeaturedId: 0,
+  secondFeaturedId: 1,
+};
+
+export const posts = [
+  {
+    id: 0,
+    title: "Test Post",
+    fileName: "test-post",
+    author: "",
+    datePublished: "",
+  },
+  {
+    id: 1,
+    title: "First Post",
+    fileName: "first-post",
+    author: "",
+    datePublished: "",
+  },
+  {
+    id: 2,
+    title: "Second Post",
+    fileName: "second-post",
+    author: "",
+    datePublished: "",
+  },
+  {
+    id: 3,
+    title: "Third Post",
+    fileName: "third-post",
+    author: "",
+    datePublished: "",
+  },
+];
