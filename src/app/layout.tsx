0f2e2ffe2f3b92b5f87cd89acc23739cb91f0795
ChrisMillerpy <christophorusmiller@gmail.com>
2024-07-26 16:37:19 +0100

    Moved some jsx from root page to root layout to rebalance what comes from where

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import BackgroundSpotlights from "@/components/Spotlight";
+import Contact from "@/components/Contact";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Chris Miller's Personal Website",
-  description: "Personal Website for Chris Miller, here you can find links to my published projects, contact page and a blog",
+  description:
+    "Personal Website for Chris Miller, here you can find links to my published projects, contact page and a blog",
 };
 
 export default function RootLayout({
@@ -14,9 +18,18 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const padding: string = "xl:px-56 lg:px-32 px-10";
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-darkred-background overflow-hidden`}
+      >
+        <Header padding={padding} />
+        <main className={`flex flex-col min-h-screen ${padding}`}>
+          {children}
+        </main>
+        <Contact />
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,17 @@
 import Blog from "@/components/Blog";
-import Contact from "@/components/Contact";
-import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import BackgroundSpotlights from "@/components/Spotlight";
 
 export default function Home() {
-  const padding: string = "xl:px-56 lg:px-32 px-10";
   return (
     <>
-      <main
-        className={`flex flex-col min-h-screen bg-darkred-background ${padding}`}
-      >
-        <Header padding={padding} />
-        <BackgroundSpotlights />
-        <div className="z-10">
-          <Hero />
-          <Projects />
-          <Blog />
-        </div>
-      </main>
-      <Contact />
+      <BackgroundSpotlights />
+      <div className="z-10">
+        <Hero />
+        <Projects />
+        <Blog />
+      </div>
     </>
   );
 }
